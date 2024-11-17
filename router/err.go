0d6eb8ec63e6801385cc2aeb0f8f49641c24cdf1
yohobala <762100246@qq.com>
2024-11-17 16:55:36 +0800

    Delete 删除不再使用的错误处理和路由相关代码

--- router/err.go
+++
@@ -1,9 +0,0 @@
-package router
-
-const text_panic_rouet_nil = "router参数不能为nil"
-
-const text_panic_router_register_twice = "不能在同一个服务下注册两个相同名字的路由,路由名: "
-
-const text_panic_server_not_register = "该服务没有注册过路由"
-
-const text_panic_server_init_twice = "不能初始化两次服务"
